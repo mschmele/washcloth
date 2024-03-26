635cb67e90ba38c95c69853bee93a9d31f34acf2
Mark Schmele <MSchmele@gmail.com>
2024-03-26 14:44:58 -0600

    load chunks into chroma

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # washcloth
 It's a RAG.
+
+## Answer these questions
+* Which two companies created the R.31 reconnaissance aircraft?
+* What guns were mounted on the Renard R.31?
+* Who was the first softball player to represent any country at four World Series of Softball?
+* Who were the pitchers on the Australian softball team's roster at the 2020 Summer Olympics?
---
+++ load_vector_db.py
@@ -0,0 +1,53 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores.chroma import Chroma
+import os
+import shutil
+from getpass import getpass
+
+OPENAI_API_KEY = getpass()
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+CHROMA_PATH = "chroma"
+OpenAIKey_Path = "openaikey.txt"
+
+
+def main():
+    documents = load_documents()
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
+    
+
+
+def load_documents():
+    loader = DirectoryLoader("data/", glob="*.pdf")
+    documents = loader.load()
+    return documents
+
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    return text_splitter.split_documents(documents)
+
+
+def save_to_chroma(chunks: list[Document]):
+    # Make sure db is clear
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+
+if __name__ == "__main__":
+    main()
